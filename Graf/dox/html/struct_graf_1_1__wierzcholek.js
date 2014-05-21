103887862a1e56bfc0963442a27bea95390f6491
Arek <200367@student.pwr.edu.pl>
2014-05-21 15:55:30 -0700

    LAB9

--- Graf/dox/html/struct_graf_1_1__wierzcholek.js
+++ Graf/dox/html/struct_graf_1_1__wierzcholek.js
@@ -2,5 +2,11 @@ var struct_graf_1_1__wierzcholek =
 [
     [ "_czy_istnieje", "struct_graf_1_1__wierzcholek.html#a157d209583d1813f1a609cfb08682ffe", null ],
     [ "_czy_odwiedzony", "struct_graf_1_1__wierzcholek.html#a818c370b6e8a476f6fb9f89ccac01e2f", null ],
-    [ "_numer", "struct_graf_1_1__wierzcholek.html#a966ecd51ba3e337c8b67587cffab7f9a", null ]
+    [ "_F", "struct_graf_1_1__wierzcholek.html#a2b1a8e8fbeb43a968c2dcfed212a4318", null ],
+    [ "_G", "struct_graf_1_1__wierzcholek.html#a2a704ec25dec8c72c17449cc4b9ab8bb", null ],
+    [ "_H", "struct_graf_1_1__wierzcholek.html#a70af2c227ab7c1b38106fb84c005da7e", null ],
+    [ "_numer", "struct_graf_1_1__wierzcholek.html#a966ecd51ba3e337c8b67587cffab7f9a", null ],
+    [ "_rodzic", "struct_graf_1_1__wierzcholek.html#a40e49e554e1b5071ea756d68a7ad06d0", null ],
+    [ "_wsp_x", "struct_graf_1_1__wierzcholek.html#a21ade81ba66bf560548f2ed5d09932e0", null ],
+    [ "_wsp_y", "struct_graf_1_1__wierzcholek.html#a8480b0d0566935fa8687c6dd7b8fa94a", null ]
 ];
